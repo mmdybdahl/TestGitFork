b9608a503c220acda5da8a57cb80b65ec2cb3bb4
Margrethe Dybdahl <mad@eaaa.dk>
2022-02-04 13:30:45 +0100

    Unit test tilføjet

---
+++ test/model/PersonTest.java
@@ -0,0 +1,27 @@
+package model;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    Person person;
+
+    @BeforeEach
+    void setUp() {
+        person = new Person("Hugo");
+    }
+
+    @Test
+    void getNavn() {
+        assertEquals("Hugo", person.getNavn());
+    }
+
+    @Test
+    void setNavn() {
+        person.setNavn("Abe");
+        assertEquals("Abe", person.getNavn());
+
+    }
+}
