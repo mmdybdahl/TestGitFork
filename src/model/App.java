6e554661c8b89729bcf1f90d914d132b13731574
Margrethe Dybdahl <mad@eaaa.dk>
2022-02-04 13:25:07 +0100

    Første commit

---
+++ src/model/App.java
@@ -0,0 +1,9 @@
+package model;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello fork");
+        Person p = new Person("Hugo");
+        System.out.println(p.getNavn());
+    }
+}
---
+++ src/model/Person.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Person {
+    private String navn;
+
+    public Person(String navn) {
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+}
